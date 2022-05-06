a721a31ba372b82a4ac117690c11e44fcd87a500
MMaidul <maidulmuqtadir360@gmail.com>
2022-05-06 22:53:08 +0600

    access JSON File,Modify,delete

---
+++ index.js
@@ -0,0 +1,34 @@
+const data = require("./friends_data.json");
+// console.log(data);
+
+//access data;
+// console.log(data.Firends);
+// first frineds data access;
+// console.log(data.Firends[0]);
+//first friends name access
+// console.log(data.Firends[0].name);
+
+
+//modify data
+// console.log(data.Firends[0].name = "Mehedi Miraz");
+
+//delete first frined
+// delete data.Firends[0].name
+// console.log(data);
+
+/* 
+----------------------------------------------
+easiest get data path using json path finder website
+https://jsonpathfinder.com/
+root name hisabe x ney ei x bad diya diya.Root name amra nijeder icca mto dibo tarpor
+.(dot) theke copy kore nile path kaj korbe;
+ei tah use kore complex path ber kora onk sohoz hoye jabe;
+------------------------------------------------------
+ */
+
+
+// const studentsData = require("./students_data.json");
+// console.log(studentsData.students[0].languages[1]);
+
+
+
